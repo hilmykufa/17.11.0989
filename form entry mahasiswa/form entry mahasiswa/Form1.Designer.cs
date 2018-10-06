9d4abd33fb46b5fbe71aad64649f851acd7cde93
hilmykufa <43564211+hilmykufa@users.noreply.github.com>
2018-10-07 00:43:11 +0700

    Tugas 3 Pemograman lanjut

---
+++ form entry mahasiswa/form entry mahasiswa/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace form_entry_mahasiswa
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lvwMahasiswa = new System.Windows.Forms.ListView();
+            this.btnTambah = new System.Windows.Forms.Button();
+            this.btnUbah = new System.Windows.Forms.Button();
+            this.btnHapus = new System.Windows.Forms.Button();
+            this.btnTutup = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lvwMahasiswa
+            // 
+            this.lvwMahasiswa.Location = new System.Drawing.Point(12, 12);
+            this.lvwMahasiswa.Name = "lvwMahasiswa";
+            this.lvwMahasiswa.Size = new System.Drawing.Size(590, 276);
+            this.lvwMahasiswa.TabIndex = 0;
+            this.lvwMahasiswa.UseCompatibleStateImageBehavior = false;
+            this.lvwMahasiswa.DoubleClick += new System.EventHandler(this.lvwMahasiswa_DoubleClick);
+            // 
+            // btnTambah
+            // 
+            this.btnTambah.Location = new System.Drawing.Point(13, 295);
+            this.btnTambah.Name = "btnTambah";
+            this.btnTambah.Size = new System.Drawing.Size(75, 23);
+            this.btnTambah.TabIndex = 1;
+            this.btnTambah.Text = "Tambah";
+            this.btnTambah.UseVisualStyleBackColor = true;
+            this.btnTambah.Click += new System.EventHandler(this.btnTambah_Click);
+            // 
+            // btnUbah
+            // 
+            this.btnUbah.Location = new System.Drawing.Point(104, 295);
+            this.btnUbah.Name = "btnUbah";
+            this.btnUbah.Size = new System.Drawing.Size(75, 23);
+            this.btnUbah.TabIndex = 1;
+            this.btnUbah.Text = "Ubah";
+            this.btnUbah.UseVisualStyleBackColor = true;
+            this.btnUbah.Click += new System.EventHandler(this.btnUbah_Click);
+            // 
+            // btnHapus
+            // 
+            this.btnHapus.Location = new System.Drawing.Point(185, 295);
+            this.btnHapus.Name = "btnHapus";
+            this.btnHapus.Size = new System.Drawing.Size(75, 23);
+            this.btnHapus.TabIndex = 1;
+            this.btnHapus.Text = "Hapus";
+            this.btnHapus.UseVisualStyleBackColor = true;
+            this.btnHapus.Click += new System.EventHandler(this.btnHapus_Click);
+            // 
+            // btnTutup
+            // 
+            this.btnTutup.Location = new System.Drawing.Point(527, 295);
+            this.btnTutup.Name = "btnTutup";
+            this.btnTutup.Size = new System.Drawing.Size(75, 23);
+            this.btnTutup.TabIndex = 1;
+            this.btnTutup.Text = "Tutup";
+            this.btnTutup.UseVisualStyleBackColor = true;
+            this.btnTutup.Click += new System.EventHandler(this.btnTutup_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(614, 327);
+            this.Controls.Add(this.btnTutup);
+            this.Controls.Add(this.btnHapus);
+            this.Controls.Add(this.btnUbah);
+            this.Controls.Add(this.btnTambah);
+            this.Controls.Add(this.lvwMahasiswa);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Data mahasiswa";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lvwMahasiswa;
+        private System.Windows.Forms.Button btnTambah;
+        private System.Windows.Forms.Button btnUbah;
+        private System.Windows.Forms.Button btnHapus;
+        private System.Windows.Forms.Button btnTutup;
+    }
+}
+
---
+++ form entry mahasiswa/form entry mahasiswa/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace form_entry_mahasiswa
+{
+    public partial class Form1 : Form
+    {
+        private IList<Mahasiswa> listOfMahasiswa = new List<Mahasiswa>();
+        private Frmmhs frmEntry;
+        public Form1()
+        {
+            InitializeComponent();
+            InisialisasiListView();
+            InisialisasiDataDummy();
+           
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            InisialisasiListView();
+            InisialisasiDataDummy();
+        }
+      
+        private void InisialisasiListView()
+        {
+            lvwMahasiswa.View = System.Windows.Forms.View.Details;
+            lvwMahasiswa.FullRowSelect = true;
+            lvwMahasiswa.GridLines = true;
+
+            lvwMahasiswa.Columns.Add("No.", 30, HorizontalAlignment.Center);
+            lvwMahasiswa.Columns.Add("Nim", 91, HorizontalAlignment.Left);
+            lvwMahasiswa.Columns.Add("Name", 200, HorizontalAlignment.Left);
+            lvwMahasiswa.Columns.Add("Jenis Kelamin", 90, HorizontalAlignment.Left);
+            lvwMahasiswa.Columns.Add("Tempat lahir", 200, HorizontalAlignment.Left);
+            lvwMahasiswa.Columns.Add("Tanggal lahir", 91, HorizontalAlignment.Left);
+        }
+
+        private void InisialisasiDataDummy()
+        {
+            listOfMahasiswa.Add(new Mahasiswa { Nim = "10.11.0001", Name = "BUDI", Gender = "Laki-laki", Alamat="JAKARTA",Date="10/10/1990" });
+            listOfMahasiswa.Add(new Mahasiswa { Nim = "17.11.0002", Name = "SANTI", Gender = "Perempuan",Alamat="JAKARTA",Date="10/10/1990" });
+           
+
+            foreach (var obj in listOfMahasiswa)
+            {
+                FillToListView(true, obj);
+            }
+        }
+
+      
+        private void FillToListView(bool isNewData, Mahasiswa mhs)
+        {
+            if (isNewData) 
+            {
+                int noUrut = lvwMahasiswa.Items.Count + 1;
+
+                ListViewItem item = new ListViewItem(noUrut.ToString());
+                item.SubItems.Add(mhs.Nim);
+                item.SubItems.Add(mhs.Name);
+                item.SubItems.Add(mhs.Gender);
+                item.SubItems.Add(mhs.Alamat);
+                item.SubItems.Add(mhs.Date);
+
+                lvwMahasiswa.Items.Add(item);
+            }
+            else 
+            {
+                int row = lvwMahasiswa.SelectedIndices[0];
+
+                ListViewItem itemRow = lvwMahasiswa.Items[row];
+                itemRow.SubItems[1].Text = mhs.Nim;
+                itemRow.SubItems[2].Text = mhs.Name;
+                itemRow.SubItems[3].Text = mhs.Gender;
+                itemRow.SubItems[4].Text = mhs.Alamat;
+                itemRow.SubItems[5].Text = mhs.Date;
+            }
+        }
+
+        private void btnTambah_Click(object sender, EventArgs e)
+        {
+            frmEntry = new Frmmhs("Tambah Data Mahasiswa");
+            frmEntry.OnSave += Frmmhs_OnSave; 
+            frmEntry.ShowDialog();
+        }
+        
+        private void Frmmhs_OnSave(Mahasiswa obj)
+        {
+            listOfMahasiswa.Add(obj);
+            FillToListView(true, obj);
+        }
+
+        private void btnUbah_Click(object sender, EventArgs e)
+        {
+            if (lvwMahasiswa.SelectedItems.Count > 0)
+            {
+                var mhs = listOfMahasiswa[lvwMahasiswa.SelectedIndices[0]];
+                
+                frmEntry = new Frmmhs("Edit Data Mahasiswa", mhs);
+
+                frmEntry.OnUpdate += Frmmhs_OnUpdate;
+           
+                frmEntry.ShowDialog();
+            }
+            else
+            {
+                MessageBox.Show("Data belum dipilih", "Peringatan", MessageBoxButtons.OK,
+                        MessageBoxIcon.Exclamation);
+            }
+        }
+        private void Frmmhs_OnUpdate(Mahasiswa obj)
+        {
+            FillToListView(false, obj);
+        }
+        private void btnHapus_Click(object sender, EventArgs e)
+        {
+            if (lvwMahasiswa.SelectedItems.Count > 0)
+            {
+                var mhs = listOfMahasiswa[lvwMahasiswa.SelectedIndices[0]];
+
+                var msg = string.Format("Apakah data mahasiswa '{0}' ingin dihapus ?", mhs.Name);
+
+                if (MessageBox.Show(msg, "Konfirmasi", MessageBoxButtons.YesNo,
+                        MessageBoxIcon.Exclamation) == DialogResult.Yes)
+                {
+                    listOfMahasiswa.Remove(mhs); 
+
+                    lvwMahasiswa.Items.Clear();
+                    foreach (var obj in listOfMahasiswa)
+                    {
+                        FillToListView(true, obj);
+                    }
+                }
+            }
+            else 
+            {
+                MessageBox.Show("Data belum dipilih", "Peringatan", MessageBoxButtons.OK,
+                        MessageBoxIcon.Exclamation);
+            }
+        }
+
+        private void btnTutup_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void lvwMahasiswa_DoubleClick(object sender, EventArgs e)
+        {
+            if (lvwMahasiswa.SelectedItems.Count > 0)
+            {
+                var mhs = listOfMahasiswa[lvwMahasiswa.SelectedIndices[0]];
+
+                frmEntry = new Frmmhs("Edit Data Mahasiswa", mhs);
+                frmEntry.OnUpdate += Frmmhs_OnUpdate;
+                frmEntry.ShowDialog();
+            }
+            else
+            {
+                MessageBox.Show("Data belum dipilih", "Peringatan", MessageBoxButtons.OK,
+                        MessageBoxIcon.Exclamation);
+            }
+        }
+    }
+}
